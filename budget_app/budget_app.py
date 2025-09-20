e82d8848538efd2065e20597f35b6dfbf3a400ef
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-09-20 16:51:05 +0100

    Created the create_spend_chart function that generates a bar chart showing the percentage spent by each category

--- budget_app/budget_app.py
+++ budget_app/budget_app.py
@@ -68,3 +68,61 @@ print(food.ledger, '\n')
 print(food.get_balance(),'\n')
 print(clothing.ledger,'\n')
 print(food)
+
+def create_spend_chart(categories): 
+
+    title = 'Percentage spent by category' 
+    
+    percentages = [] 
+    for i in range(10, -1, -1):
+        percentage = i*10
+        percentage_str = str(percentage).rjust(3) + '|'
+        percentages.append(percentage_str) 
+     
+    total_withdrawals = 0 
+    for cat in categories: 
+        total_withdrawals_per_cat = 0 
+        for i in cat.ledger: 
+            total_withdrawals_per_cat += i['amount'] if i['amount'] < 0 and not i['description'].startswith('Transfer') else 0 
+        total_withdrawals += -total_withdrawals_per_cat
+
+    characters = [] 
+    cat_percentages = [] 
+    for cat in categories: 
+        sum_cat = 0 
+        for i in cat.ledger: 
+            sum_cat += -i['amount'] if i['amount'] < 0 and not i['description'].startswith('Transfer') else 0 
+        percentage = (sum_cat/total_withdrawals)*100 
+        cat_percentages.append(percentage) 
+        characters.append('o'*(int(percentage//10))) 
+    
+    max_characters = max(len(i) for i in characters) 
+    adjusted = [i.rjust(max_characters) for i in characters] 
+    zipped = list(zip(*adjusted)) 
+    characters_list = [] 
+    for i in zipped: 
+        characters_list.append(' '.join(i)) 
+    
+    combined_list = list(zip(percentages,characters_list))
+    combined_final = []
+    for i in combined_list:
+        combined_final.append(i[0] + ' ' + i[1]) 
+    columns = '\n'.join(combined_final)
+
+    num_dashes = 3*len(categories)+1
+    dashes = '    '+'-'*num_dashes
+
+    names_list = []
+    for cat in categories:
+        names_list.append(cat.name)
+    max_name_characters = max(len(i) for i in names_list)
+    names_adjusted = [i.ljust(max_name_characters) for i in names_list]
+    names_zipped = list(zip(*names_adjusted))
+    names_line = []
+    for i in names_zipped:
+        names_line.append('  '.join(i))
+    names = '\n'.join('     ' + line for line in names_line)
+
+    return title+'\n'+columns+'\n'+dashes+'\n'+names
+
+print(create_spend_chart([food, clothing]))
