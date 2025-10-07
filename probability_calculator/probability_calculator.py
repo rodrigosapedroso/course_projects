2a365215e062b864f208022105716af2edafcfce
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-10-07 22:34:59 +0100

    implement draw method to randomly remove items from hat contents

--- probability_calculator/probability_calculator.py
+++ probability_calculator/probability_calculator.py
@@ -1,3 +1,5 @@
+import random
+
 class Hat:
     def __init__(self, **color_quant):
         contents = []
@@ -5,5 +7,19 @@ class Hat:
             for i in range(quant):
                 contents.append(color)
         self.contents = contents
+    
+    def __repr__(self):
+        return f'{self.__class__.__name__}({self.contents})'
+    
+    def draw(self, number):
+        if number > len(self.contents):
+            return self.contents
+        drawback = random.sample(self.contents, number)
+        for i in drawback:
+            self.contents.remove(i)
+        return drawback          
+
 
 hat1 = Hat(red=2, blue=1)
+print(hat1)
+print(hat1.draw(2))
