65c9d9d93f91f2286237aa3be69c4b637d9b7fce
Rodrigo Pedroso <rodrigosapedroso@gmail.com>
2025-10-10 00:53:56 +0100

    implemented experiment function to estimate probability using an iterative approach over multiple trials

--- probability_calculator/probability_calculator.py
+++ probability_calculator/probability_calculator.py
@@ -1,3 +1,4 @@
+import copy
 import random
 
 class Hat:
@@ -16,13 +17,35 @@ class Hat:
             all = self.contents.copy()
             self.contents = []
             return all
-        else:
-            drawback = random.sample(self.contents, number)
-            for i in drawback:
-                self.contents.remove(i)
-            return drawback             
+        drawback = random.sample(self.contents, number)
+        for i in drawback:
+            self.contents.remove(i)
+        return drawback          
 
 
-hat1 = Hat(red=2, blue=1)
+hat1 = Hat(black=6, red=4, green=3)
 print(hat1)
-print(hat1.draw(2))
+print(hat1.draw(5))
+
+def experiment(hat, expected_balls, num_balls_drawn, num_experiments):
+    
+    N = num_experiments
+    M = 0
+    
+    for i in range(N):
+        cases = []
+        hat_copy = copy.deepcopy(hat)
+        l_drawn = hat_copy.draw(num_balls_drawn)
+        for color, quant in expected_balls.items():
+            if l_drawn.count(color) >= quant:
+                cases.append('yes')
+            else:
+                cases.append('no')
+        if all(i == 'yes' for i in cases):
+            M += 1
+    
+    prob = M/N
+    return prob
+
+probability = experiment(hat1, {'red':2,'green':1}, 5, 2000)
+print(probability)
